5164918335c4dd7a80d8e95696722dcb25d941b0
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-05-20 23:22:39 +0100

    Create marketing banner;

---
+++ components/marketing/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="antialiased rounded shadow flex flex-wrap m-16 bg-white">
+    <div class="bg-local bg-cover w-1/2" style="background-image: url('/imgs/pycon.jpg')" />
+
+    <div class="w-1/2 py-12 px-8">
+      <h1 class="text-3xl mb-4 font-extrabold">
+        Hi there, I'm Abdulhakeem
+      </h1>
+      <p class="text-gray-700 font-normal text-lg leading-relaxed">
+        A couple of weeks ago, I saw <a href="https://www.linkedin.com/in/pydanny"> Daniel Roy Greenfeld </a> make a post
+        about Tailwind CSS, and that was the beginning of a beautiful journey. Haven personally disliked how most sites
+        built with popular CSS frameworks look alike and generally how difficult it is to completely restyle,
+        TailwindCSS was just what I've been searching for. A few weeks later, I'm applying for a Full-Stack Developer role
+        with the awesome team behind it.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Marketing'
+}
+</script>
+
+<style scoped>
+
+</style>
