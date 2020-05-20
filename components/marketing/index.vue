3b8a6b4391d271cb6a4dc2259b959abc731b2aaf
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-05-21 00:38:25 +0100

    Add color to links

--- components/marketing/index.vue
+++ components/marketing/index.vue
@@ -7,7 +7,7 @@
         Hi there, I'm Abdulhakeem
       </h1>
       <p class="text-gray-700 font-normal text-lg leading-relaxed">
-        A couple of weeks ago, I saw <a href="https://www.linkedin.com/in/pydanny"> Daniel Roy Greenfeld </a> make a post
+        A couple of weeks ago, I saw <a href="https://www.linkedin.com/in/pydanny" class="text-teal-700"> Daniel Roy Greenfeld </a> make a post
         about Tailwind CSS, and that was the beginning of a beautiful journey. Haven personally disliked how most sites
         built with popular CSS frameworks look alike and generally how difficult it is to completely restyle,
         TailwindCSS was just what I've been searching for. A few weeks later, I'm applying for a Full-Stack Developer role
