d2661cd52e8866ebc889bdacfa6596e073247dd5
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-05-21 00:19:48 +0100

    Finish up about me

--- components/about/index.vue
+++ components/about/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="card my-4 py-12 px-8">
-    <p>
-      I was short on ideas on exactly what to put on this site, I didn't want it to be a full clone of my personal
-      resume since there's a link to that already. While I'm trying to ensure it clearly communicates my excitement,
-      skills and doesn't bore you with too much information. I figred out the best way to do that was to tell my story
-      using cards from the design tile on <a href="https://tailwindui.com/img/hero-pattern-lg.png"> Tailwind UI </a>
-    </p>
+    <div class="w-1/5">
+      <img src="https://amustapha.com/my-pic-no-bg.webp"  class="w-full rounded-full bg-gray-400"/>
+    </div>
+    <div class="w-4/5 ml-10">
+      <p class="text-xl text-gray-700 leading-relaxed font-medium">
+        I must admit, I was short on ideas on exactly what to put on this site, I didn't want it to be a full clone of my
+        portfolio since there's a link to that already. While I'm trying to ensure it clearly communicates my excitement,
+        skills and doesn't bore you with too much information. I figred out the best way to do that was to tell my story
+        using cards from the design tile on <a href="https://tailwindui.com/img/hero-pattern-lg.png" class="text-teal-700"> Tailwind UI.</a>
+      </p>
+
+      <h4 class="font-semibold text-2xl mt-6 mb-0">Abdulhakeem A. M.</h4>
+      <small class="text-teal-700 mt-0 text-lg">Future fullstack engineer, Tailwind CSS</small>
+    </div>
+
   </div>
 </template>
 
