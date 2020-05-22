2365ab752d3803a3ff8afc50f267d8f75aa9d099
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-05-22 21:33:24 +0100

    Remove blog link

--- components/marketing/index.vue
+++ components/marketing/index.vue
@@ -1,22 +1,21 @@
 <template>
   <div class=" bg-contain bg-white  bg-no-repeat bg-right mb-12 " style="background-image: url('/imgs/pycon.jpg')">
     <div
-      class="bg-cover bg-white md:bg-transparent w-full md:w-11/12 lg:w-8/12 xl:w-3/5 py-12 md:py-16 lg:py-24 xxl:py-32
+      class="bg-cover bg-white md:bg-transparent w-full md:w-10/12 lg:w-8/12 xl:w-7/12 py-12 md:py-16 lg:py-24 xxl:py-32
        pl-8 md:pl-24 lg:pl-32 xl:pl-48 pr-8 md:pr-12 xl:pr-32 bg-local"
       style="background-image: url('/imgs/angled-background.svg'); ">
       <h1 class="text-4xl mb-4 font-black">
         Hi Adam, I'm Abdulhakeem
       </h1>
-      <p class="text-gray-700 font-normal text-xl leading-relaxed">
+      <p class="text-gray-700 font-normal text-lg lg:text-xl leading-relaxed">
         A couple of weeks ago, I saw <a href="https://www.linkedin.com/in/pydanny" class="text-teal-700"> Daniel Roy
         Greenfeld </a> make a post about Tailwind CSS, and that was the beginning of a beautiful journey. I fell in love
         with TailwindCSS as it gives me the flexibility to customize my designs and also makes me deliver results quicker.
         A few weeks later, I'm applying for a Full-Stack Developer role with the awesome team behind it.
       </p>
       <div class="flex mt-8 font-bold">
-        <a href="https://blog.amustapha.com" class="btn btn-teal" target="_blank">Blog</a>
         <a href="https://docs.google.com/document/d/1xElMvl-O5xN6oA5U989hwJlEwEseLTA-QxpNFkxs73Q/edit"
-           class="btn btn-blue mx-4" target="_blank">Resume</a>
+           class="btn btn-blue mr-4" target="_blank">Resume</a>
         <a class="btn" target="_blank">Portfolio</a>
       </div>
     </div>
