f10cf658c308a7aa423fce964d7a5a03173655df
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-05-22 23:23:34 +0100

    Make header responsive

---
+++ components/profile/ProfileHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="border-b-2 w-full py-4 px-6">
+    <div class="flex items-center">
+      <div class="w-16  flex-shrink">
+        <img src="/imgs/my-pic-no-bg.png" class="w-full rounded-full bg-gray-400 mx-auto"/>
+      </div>
+      <div class="flex-grow px-4">
+        <h2 class="text-md md:text-xl font-bold">Abdulhakeem <span class="hidden md:inline">Adetunji</span> Mustapha</h2>
+        <h6 class="text-gray-600 font-semibold"><a href="https://twitter.com/_amustapha">@_amustapha</a></h6>
+
+      </div>
+      <div class="flex-shrink font-semibold">
+        <a class="btn btn-contact text-blue-400 mr-2 border-blue-400 hover:text-blue-600 hover-border-blue-600"
+           href="tel:+2347067577058">
+          <span class="icon-phone md:mr-2"/>
+          <span class="hidden md:inline"> Phone</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProfileHeader'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- components/profile/index.vue
+++ components/profile/index.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="container mx-auto mx-8">
-    <div class="w-8/12">
+    <div class="w-full xl:w-8/12">
       <div class="card flex-wrap">
-        <div class="border-b-2 w-full py-4 px-6">
-          <div class="flex items-center">
-            <div class="w-16  flex-shrink">
-              <img src="/imgs/my-pic-no-bg.png" class="w-full rounded-full bg-gray-400 mx-auto"/>
-            </div>
-            <div class="flex-grow px-4">
-              <h2 class="text-xl font-bold">Abdulhakeem Adetunji Mustapha</h2>
-              <h6 class="text-gray-600 font-semibold"><a href="https://twitter.com/_amustapha">@_amustapha</a></h6>
-
-            </div>
-            <div class="flex-shrink font-semibold">
-              <a class="btn btn-contact text-blue-400 mr-2 border-blue-400 hover:text-blue-600 hover-border-blue-600"
-                 href="tel:+2347067577058">
-                <span class="icon-phone mr-2"/>
-                Phone
-              </a>
-            </div>
-          </div>
-        </div>
+        <profile-header/>
         <div class="py-4 px-6 w-full">
           <div class="grid grid-cols-2 gap-4">
             <keyset label="Application for">Fullstack Developer</keyset>
@@ -28,13 +10,16 @@
             </keyset>
             <keyset label="About" class="col-span-2">
               <p>
-                I wrote my first line of html code sometime in March, 2014 and it has been an amazing journey ever
-                since,
+                I wrote my first bit of html code sometime in March, 2014 and it has been an amazing journey ever since,
                 filled with curiosity, accomplishments and a love/hate relationship when things don't work for no good
                 reason.
                 I've learnt that there's much more to be learnt, but I also realised that people are willing to sit with
-                me
-                to share my experience. Devout to learning, growing and helping others grow along my path.
+                me to share my experience. Devout to learning, growing and helping others grow along my path.
+              </p>
+
+              <p class="mt-3">I learnt to write code by myself, but taking every opportunity to learn from the masters.
+                I realize that above every possessor of knowledge, there's one person more learned. There is no
+                perfection, but I strive to minimize imperfection.
               </p>
 
               <p class="mt-3">I learnt to write code by myself, but taking every opportunity to learn from the masters.
@@ -57,10 +42,14 @@
 
 <script>
   import Keyset from './keyset'
+  import ProfileHeader from './ProfileHeader'
 
   export default {
     name: 'Profile',
-    components: { Keyset }
+    components: {
+      ProfileHeader,
+      Keyset
+    }
   }
 </script>
 
