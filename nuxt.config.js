f56586f8290e55089ecf7276e32475372d6861af
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-05-23 00:27:53 +0100

    Fix page meta and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Job application to Tailwind UI as Fullstack Developer' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/pic-favicon.png' }
     ]
   },
   /*
